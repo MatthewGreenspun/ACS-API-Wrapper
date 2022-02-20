98df0e46e2b263d251dcecaa622c3be111ad5488
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2022-02-20 11:02:40 -0500

    changed ways of organizing acs vars

--- varlist/sort_acs_vars.py
+++ varlist/sort_acs_vars.py
@@ -6,8 +6,9 @@ with open("varlist/acs_2019_vars.json") as vars_file:
 	vars = json.load(vars_file)
 	for (full_col, col_data) in vars.items():
 		if len(full_col.split("_")) < 2: 
+			print(f"fullcol: {full_col} col_data: {col_data}")
 			formatted_vars[full_col] = {
-				"table_name": f"{full_col} - {col_data['label']}",
+				"tableName": col_data['label'],
 				"cols": {}
 			}
 			continue
@@ -16,7 +17,7 @@ with open("varlist/acs_2019_vars.json") as vars_file:
 			formatted_vars[table]["cols"][col] = col_data["label"].replace("!!", ">").replace(":", "")
 		else: 
 			formatted_vars[table] = {
-				"table_name": col_data["concept"],
+				"tableName": col_data["concept"],
 				"cols": {}
 			}
 
@@ -27,10 +28,10 @@ num_cols = 0
 for table in sorted(formatted_vars):
 	num_tables += 1
 	sorted_vars[table] = {
-		"table_name": "", 
+		"tableName": "", 
 		"cols": {}
 	}
-	sorted_vars[table]["table_name"] = formatted_vars[table]["table_name"]
+	sorted_vars[table]["tableName"] = formatted_vars[table]["tableName"]
 	for col in sorted(formatted_vars[table]["cols"]):
 		num_cols += 1
 		sorted_vars[table]["cols"][col] = formatted_vars[table]["cols"][col]
@@ -38,5 +39,40 @@ for table in sorted(formatted_vars):
 print(f"total number of tables: {num_tables}")
 print(f"total number of columns: {num_cols}")
 
-with open("varlist/sorted_acs_2019_vars.json", "w") as outfile:
+with open("varlist/acs_2019_vars_by_tableid.json", "w") as outfile:
 	json.dump(sorted_vars, outfile, indent=2)	
+
+varlist = []
+for table_id in sorted_vars:
+	table = {
+		"tableId": table_id,
+		**sorted_vars[table_id]
+	}
+	varlist.append(table)
+
+with open("varlist/acs_2019_vars_list.json", "w") as outfile:
+	json.dump(varlist, outfile, indent=2)	
+
+# index file for tinysearch
+tinysearch_varlist = []
+title = "{var_id}-{var_name}"
+for table in varlist:
+	if table["tableId"] == "GEO":
+		continue
+	index = {
+		"title": title.format(var_id=table["tableId"], var_name=table["tableName"]),
+		"url": "",
+	}
+	tinysearch_varlist.append(index)
+	for col, col_name in table["cols"].items():
+		var_id = table["tableId"] + "_" + col
+		var_name=col_name.replace("Estimate>Total>", "").replace("Estimate>", "").replace("\"", "")
+		index = {
+			"title": title.format(var_id=var_id, var_name=var_name),
+			"url": "",
+		}
+		tinysearch_varlist.append(index)
+
+
+with open("varlist/tinysearch.json", "w") as outfile:
+	json.dump(tinysearch_varlist, outfile, indent=2)
