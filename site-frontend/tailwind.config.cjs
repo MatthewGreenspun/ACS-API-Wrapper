34b2f904e5a1368a83fe410aa9c151b067afca0b
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2022-03-27 18:35:31 -0400

    use tailwind elements

--- site-frontend/tailwind.config.cjs
+++ site-frontend/tailwind.config.cjs
@@ -1,15 +1,30 @@
 module.exports = {
-	purge: ["./src/**/*.html", "./src/**/*.svelte"],
-	darkMode: false,
+	purge: [
+		"./src/**/*.html",
+		"./src/**/*.svelte",
+		"./src/**/*.{html,js}",
+		"./node_modules/tw-elements/dist/js/**/*.js"
+	],
+	darkMode: "media",
 	theme: {
 		extend: {
 			flexGrow: {
 				2: 2
+			},
+			maxWidth: {
+				screen: "100vw",
+				"screen-1/3": "33vw",
+				"screen-2/3": "60vw",
+				"screen-90": "90vw"
+			},
+			minWidth: {
+				"screen-1/3": "33vw",
+				"screen-2/3": "66vw"
 			}
 		}
 	},
 	variants: {
 		extend: {}
 	},
-	plugins: []
+	plugins: [require("tw-elements/dist/plugin")]
 };
